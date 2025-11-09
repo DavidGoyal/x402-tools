77c9a887e7c10ce690ada431762997a715ca4f26
DavidGoyal <goyaldavid55@gmail.com>
2025-11-09 17:23:07 +0530

    added fs-extra in config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["@lighthouse-web3/sdk", "bls-eth-wasm"],
+  serverExternalPackages: ["@lighthouse-web3/sdk", "bls-eth-wasm", "fs-extra"],
   turbopack: {
     resolveAlias: {
       // Ensure proper resolution of WebAssembly modules
       "bls-eth-wasm": "bls-eth-wasm",
+      "fs-extra": "fs-extra",
     },
   },
 };
